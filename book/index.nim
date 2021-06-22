3b65ae48db93b66d3510d396bcb88bb27cdc1acd
Vindaar <basti90@gmail.com>
2021-06-22 20:45:51 +0200

    indeed!

--- book/index.nim
+++ book/index.nim
@@ -6,6 +6,8 @@ nbUseNimibook
 nbText: """
 # Introduction
 
+Woah, seriously?
+
 This is the SciNim getting started book.
 
 It's a collection of examples and code snippet to help people use the scientific libraries of the Nim programming language.
